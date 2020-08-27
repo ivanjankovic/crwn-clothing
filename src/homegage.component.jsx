d7b592623c7c6f65cebd55af2939017560aab5e4
ivan Jankovic <jankoemail@gmail.com>
2020-08-26 23:07:56 -0400

    hompage wireframe setup

--- src/homegage.component.jsx
+++ src/homegage.component.jsx
@@ -1,11 +1,45 @@
 import React from 'react';
 
-const homegage.component = () => {
+import './homepage.styles.scss'
+
+const HomePage = () => {
   return (
-    <div>
-      
+    <div className='homepage'>
+      <h1>Welcome to my Homepage</h1>
+      <div className='directory-menu'>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>HATS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>JACKETS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>SNEAKERS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>WOMENS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+        <div className='menu-item'>
+          <div className='content'>
+            <h1 className='title'>MENS</h1>
+            <span className='subtitle'>SHOP NOW</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
 
-export default homegage.component;
+export default HomePage;
