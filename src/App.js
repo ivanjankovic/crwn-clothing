259b4ae2d083f7d7cafeae5c0528d725e31a14e6
ivan Jankovic <jankoemail@gmail.com>
2020-08-28 10:27:48 -0400

    Adding header componenet and links to home, shop page and contact

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import './App.css';
 
 import HomePage from './pages/homepage/homegage.component'
 import ShopPage from './pages/shop/shop.componenet';
+import Header from './componenets/header/header.component';
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
