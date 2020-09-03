87c099f28cbff5d9aeb60a4880c3a38516e8fa4b
ivan Jankovic <jankoemail@gmail.com>
2020-09-03 10:31:19 -0400

    added redux, and implemented userReducer and userActions to header and app components

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,17 +1,17 @@
-const INITIAL_STATE = {
+const I_STATE = {
   currentUser: null
-};
+}
 
-const userReducer = (state = INITIAL_STATE, action) => {
+const userReducer = (state = I_STATE, action) => {
   switch (action.type) {
     case 'SET_CURRENT_USER':
       return {
         ...state,
         currentUser: action.payload
-      };
+      }
     default:
       return state;
   }
-};
+}
 
-export default userReducer;
+export default userReducer
